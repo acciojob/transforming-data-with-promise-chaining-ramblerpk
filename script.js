1bcbded85d8453215ac95ab9f00a8cc4a5188162
Piyush Kumar <109018436+ramblerpk@users.noreply.github.com>
2025-08-06 21:09:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,65 @@
 //your JS code here. If required.
+document.getElementById('btn').addEventListener('click', function(){
+	const inputValue = parseInt(document.getElementById('ip').value);
+	processInput(inputValue);
+});
+
+function processInput(value) {
+	new Promise((resolve)=>{
+		setTimeout(()=>{
+			document.getElementById('output').innerHTML =`Result: ${value}`;
+			resolve(value);
+		},2000);
+	})
+	.then((result)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const newResult =result*2;
+				document.getElementById('output').innerHTML =`Result: ${newResult}`;
+				resolve(newResult);
+			},2000);
+		});
+	})
+	.then((result)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const newResult = result-3;
+				document.getElementById('output').innerHTML = `Result: ${newResult}`;
+				resolve(newResult);
+			},1000);
+		});
+	})
+	.then((result)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const newResult = result/2;
+				document.getElementById('output').innerHTML = `Result: ${newResult}`;
+				resolve(newResult);
+			},1000);
+		});
+	})
+	.then((result)=>{
+		return new Promise((resolve)=>{
+			setTimeout(()=>{
+				const newResult = result+10;
+				document.getElementById('output').innerHTML = `Result: ${newResult}`;
+				resolve(newResult);
+			},1000);
+		});
+	});
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
